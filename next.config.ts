5e79bf77898b424ada6c32f22f212074896019e4
ZufarovAbdulla <zufarovabdulla08@gmail.com>
2025-01-27 14:08:18 +0500

    image uchun hostname qo'shildi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'https://ashyo.store',
+      },]
+    }
 };
 
 export default nextConfig;
